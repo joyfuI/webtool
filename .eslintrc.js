ab0574802fe6807758bc6cbc62008802dca071a2
joyfuI <1342862+joyfuI@users.noreply.github.com>
2024-08-17 22:46:03 +0900

    typescript-eslint 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-  extends: ['eslint:recommended', 'next/core-web-vitals', 'prettier'],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'no-unused-vars': ['warn', { ignoreRestSiblings: true }], // 미사용 변수 경고. https://eslint.org/docs/v8.x//rules/no-unused-vars
+    '@typescript-eslint/no-unused-vars': 'warn', // 미사용 변수 경고. https://typescript-eslint.io/rules/no-unused-vars
   },
 };
