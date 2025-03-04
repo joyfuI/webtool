7fa596d00774145275e217ea36a9320c4af2953f
joyfuI <1342862+joyfuI@users.noreply.github.com>
2025-03-05 00:57:40 +0900

    nuqs 패키지 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react';
 import type { Metadata } from 'next';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
@@ -23,16 +24,18 @@ const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <html lang="ko" suppressHydrationWarning>
       <body>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <InitColorSchemeScript attribute="class" />
-            <CssBaseline />
-            <Header />
-            <Container component="main" sx={{ pt: 2, pb: 4 }}>
-              {children}
-            </Container>
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+        <NuqsAdapter>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+              <InitColorSchemeScript attribute="class" />
+              <CssBaseline />
+              <Header />
+              <Container component="main" sx={{ pt: 2, pb: 4 }}>
+                {children}
+              </Container>
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
