5ebb5088bb6857f5945bda7b6167c3922e3a0277
joyfuI <1342862+joyfuI@users.noreply.github.com>
2025-06-30 22:54:57 +0900

    ffmpeg에 속도 조절 추가

---
+++ src/app/ffmpeg/command/Videoaudio/Timestretch.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { useState } from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import Input from '@mui/material/Input';
+import Stack from '@mui/material/Stack';
+
+import CommandCopy from '@/components/CommandCopy';
+import useToggle from '@/hooks/useToggle';
+
+import type { DefaultCommandProps } from '../../logic';
+
+const Timestretch = ({ command, input, output }: DefaultCommandProps) => {
+  const [speed, setSpeed] = useState(0.5);
+  const [keepPitch, toggleKeepPitch] = useToggle(true);
+
+  return (
+    <CommandCopy
+      args={{
+        '-i': `"${input}"`,
+        '-filter_complex': `"[0:v]setpts=${speed ? 1 / speed : 0}*PTS[v];[0:a]rubberband=tempo=${speed}:pitch=${keepPitch ? 1 : speed}[a]"`,
+        '-map': '"[v]" -map "[a]"',
+        '': `"${output}"`,
+      }}
+      command={command}
+      label="속도 조절"
+    >
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={{ xs: 1, sm: 5 }}
+        sx={{ mb: 1, alignItems: 'flex-end' }}
+      >
+        <Stack>
+          <FormLabel sx={{ fontSize: 14 }}>배속</FormLabel>
+          <Input
+            onChange={(e) => setSpeed(Number.parseFloat(e.target.value))}
+            slotProps={{ input: { min: 0.1, step: 0.1, inputMode: 'numeric' } }}
+            sx={{ width: '60px' }}
+            type="number"
+            value={Number.isNaN(speed) ? '' : speed}
+          />
+        </Stack>
+        <FormControlLabel
+          checked={keepPitch}
+          control={<Checkbox onChange={toggleKeepPitch} />}
+          label="피치 유지"
+        />
+      </Stack>
+    </CommandCopy>
+  );
+};
+
+export default Timestretch;
--- src/app/ffmpeg/command/Videoaudio/index.tsx
+++ src/app/ffmpeg/command/Videoaudio/index.tsx
@@ -6,6 +6,7 @@ import Concat from './Concat';
 import Copy from './Copy';
 import Merge from './Merge';
 import Reverse from './Reverse';
+import Timestretch from './Timestretch';
 import Trim from './Trim';
 
 const Videoaudio = ({ command, input, output }: DefaultCommandProps) => {
@@ -13,8 +14,9 @@ const Videoaudio = ({ command, input, output }: DefaultCommandProps) => {
     <TabPanel value="videoaudio">
       <Stack spacing={3}>
         <Trim command={command} input={input} output={output} />
-        <Copy command={command} input={input} output={output} />
         <Reverse command={command} input={input} output={output} />
+        <Timestretch command={command} input={input} output={output} />
+        <Copy command={command} input={input} output={output} />
         <Concat command={command} input={input} output={output} />
         <Merge command={command} input={input} output={output} />
       </Stack>
